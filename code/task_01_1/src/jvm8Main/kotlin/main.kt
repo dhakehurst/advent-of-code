71bed4ee3b5c52c6161a9b9ece68c825a247d0c8
Dr D H Akehurst <dhakehurst@users.noreply.github.com>
2023-12-01 09:11:11 +0100

    init

---
+++ code/task_01_1/src/jvm8Main/kotlin/main.kt
@@ -0,0 +1,20 @@
+import korlibs.io.file.std.resourcesVfs
+
+suspend fun main() {
+    val lines = resourcesVfs["input.txt"].readLines().toList()
+    var total = 0
+    for(line in lines) {
+        var first = 0
+        var last = 0
+        line.forEach {
+            when{
+                it in '0'..'9' -> {
+                    if (0==first) first = it.digitToInt()
+                    last = it.digitToInt()
+                }
+            }
+        }
+       total += (first*10)+last
+    }
+    println(total)
+}
